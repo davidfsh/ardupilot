555435ae1758dcfe5fe9b9d8f965f53f5c96528d
davidfsh <fengsh@umich.edu>
2017-09-06 22:14:59 -0400

    Add files via upload
    add AP_Beacon_Ultrasound.cpp and AP_Beacon_Ultrasound.h

---
+++ libraries/AP_Beacon/AP_Beacon_Ultrasound.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "AP_Beacon_Backend.h"
+
+#define BUFFER_SIZE 5
+#define TRIGGER_CHANNEL CH5
+#define ECHO_LEFT CH6
+#define ECHO_RIGHT CH7
+
+const AP_HAL::HAL& hal = AP_HAL::get_HAL();
+
+class AP_Beacon_Ultrasound : public AP_Beacon_Backend
+{
+public:
+			//Constructor for AP_Beacon_Ultrasound
+			AP_Beacon_Ultrasound(AP_Beacon &frontend);
+													
+			//Check whether device is healthy
+			bool healthy();
+	
+			//Update data from the ultrasonic sensor
+			void update();
+	
+private:
+			AP_HAL::UARTDriver *_xbee=hal.uartF;
+			AP_HAL::RCOutput *_trigger=hal.rcout;
+			AP_HAL::RCInput *_echo=hal.rcin;
+			
+			uint32_t last_update_ms = 0;
+			uint16_t last_left_data=0;
+			uint32_t last_right_data=0;
+			
+			//This function translate the echo signal into a distance number;
+			uint16_t _dis_calculation(uint32_t echo_time);
+			
+			//Ring Buffer for storing the data read from ultrasonic sensor;
+			uint16_t left_ring[BUFFER_SIZE];
+			uint16_t right_ring[BUFFER_SIZE];
+			uint8_t size_of_ring=0;
+			uint8_t index=0; //Index of the next reading
+			
+			uint16_t left_echo=0;
+			uint16_t right_echo=0;
+}
