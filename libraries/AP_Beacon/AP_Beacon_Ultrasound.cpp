20f6c40e2f72394b1bb3d7c63ec105e2492ce12b
fengsh <fengsh@umich.edu>
2017-09-10 21:07:12 -0400

    For loop modified and comment added

--- libraries/AP_Beacon/AP_Beacon_Ultrasound.cpp
+++ libraries/AP_Beacon/AP_Beacon_Ultrasound.cpp
@@ -12,7 +12,7 @@ AP_Beacon_Ultrasound::AP_Beacon_Ultrasound(AP_Beacon &frontend):
 // return true if sensor is bascially healthy (we are receiving data)
 bool AP_Beacon_Ultrasound::healthy()
 {
-	// healthy if we have get a data in the past 300ms
+	// healthy if we have get a data in the past 300ms, I am not sure whether we need to shrink this time check threshold, since the period of ultraonic sensor update is 25ms.
 	return ((AP_HAL::millis() - last_update_ms) < AP_BEACON_TIMEOUT_MS);
 }
 
@@ -34,20 +34,21 @@ void AP_Beacon_Ultrasound::update()
 	if (size_of_ring<BUFFER_SIZE) size_of_ring++;
     
     //TODO: Shi Hao, could you comment this section to make it a bit
-    //clearer? I don't know what this for loop is doing.
-	int temp_sum_left=0; int temp_sum_right=0; int shift_index=0;
+    //clearer? I don't know what this for loop is doing
+    //This is a ring buffer to filter the reading of ultrasonic sensor a little bit. It seems that I do some silly processing here. My original idea is to sum up the valid numbers in the ring buffer from the oldest value. However, it turns out to be both wrong and unnecassary. 
+	int temp_sum_left=0; int temp_sum_right=0; //int shift_index=0;
 	for (int i=0; i<size_of_ring; i++)
 	{
-		if (index>=i) 
+		/*if (index>=i) 
 		{
 			shift_index=index-i;
 		}
 		else 
 		{
 			shift_index=index+BUFFER_SIZE-i;
-		}
-		temp_sum_left+=left_ring[shift_index];
-		temp_sum_right+=right_ring[shift_index];
+		}*/
+		temp_sum_left+=left_ring[i];
+		temp_sum_right+=right_ring[i];
 	} 
 	last_left_data = temp_sum_left / size_of_ring;
 	last_right_data = temp_sum_right / size_of_ring;
